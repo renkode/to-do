fd4c7fbb7ad5ce4647b6c0af646c4c44318228b7
Ren Tran <zaraku2@live.com>
2021-10-12 01:45:08 -0700

    added top bar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './style.css';
+import './colors.css';
 import '../node_modules/jquery/dist/jquery.slim.min.js';
 import '../node_modules/popper.js/dist/umd/popper.min.js';
 import '../node_modules/bootstrap/dist/js/bootstrap.min.js';
